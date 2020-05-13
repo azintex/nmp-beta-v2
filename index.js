c11ff26a7bfb04f78658035df97ba19433e0c283
Haji Uzdenov <h.uzdenov@azintex.com>
2020-05-13 20:01:44 +0400

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {render} from 'react-dom';
+import App from './src/App';
+import {Provider} from 'react-redux';
+import {store} from './src/_helpers/store';
+import 'bootstrap';
+import './styles.scss';
+
+//store.subscribe(() => console.log(store.getState()));
+
+render(<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: ['babel-polyfill', './index.js'],
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.(scss)$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: function() {
+                                return [require('autoprefixer')]
+                            }
+                        }
+                    },
+                    {loader: 'sass-loader'}
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ],
+    devServer: {
+        historyApiFallback: true,
+        contentBase: path.resolve(__dirname, 'public'),
+        port: 8000
+    },
+    externals: {
+        config: JSON.stringify({
+            apiUrl: 'https://api.azintex.com/v1'
+        })
+    }
+}

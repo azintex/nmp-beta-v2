db733341ec3a54091b28d91f64f96dfc4a804369
Haji Uzdenov <h.uzdenov@azintex.com>
2020-05-21 16:10:19 +0400

    minor changes

---
+++ src/components/Connection/SubscriberType.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+
+function ChooseSubscriberType() {
+    return(
+        <div className='col-9'>
+            <div className='card'>
+                <div className='card-header'>
+                    <Evaluation stage={1} label={'CUSTOMER'} />
+                </div>
+            </div>
+        </div>
+    )
+}

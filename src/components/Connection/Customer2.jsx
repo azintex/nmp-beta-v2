2d71c1cc98c6d57c0fa9f1ff4b5212776a26da96
Haji Uzdenov <h.uzdenov@azintex.com>
2020-05-27 15:34:25 +0400

    subscriber with formik

--- src/components/Connection/Customer2.jsx
+++ src/components/Connection/Customer2.jsx
@@ -3,28 +3,43 @@ import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 const SignupSchema = Yup.object().shape({
+  idNumber: Yup.number()
+    .required('Required'),
   firstName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
     .required('Required')
     .matches(/[a-z]/i, 'Only letters allowed'),
   lastName: Yup.string()
-    .min(2, 'Too Short!')
-    .max(50, 'Too Long!')
+    .required('Required')
+    .matches(/[a-z]/i, 'Only letters allowed'),
+  patronymic: Yup.string()
+    .required('Required')
+    .matches(/[a-z]/i, 'Only letters allowed'),
+  birthPlace: Yup.string()
+    .required('Required')
+    .matches(/[a-z]/i, 'Only letters allowed'),
+  birthDate: Yup.date()
+    .required('Required'),
+  phone: Yup.number()
     .required('Required'),
   email: Yup.string()
-    .email('Invalid email')
-    .required('Required'),
+    .email('Not valid email'),
+  rate: Yup.number()
+    .required('Required')
 });
 
 export const Customer2 = () => (
   <div>
-    <h1>Signup</h1>
     <Formik
       initialValues={{
+        idNumber: '',
         firstName: '',
         lastName: '',
+        patronymic: '',
+        birthPlace: '',
+        birthDate: '',
+        phone: '',
         email: '',
+        rate: ''
       }}
       validationSchema={SignupSchema}
       onSubmit={values => {
@@ -34,14 +49,51 @@ export const Customer2 = () => (
     >
       {({ errors, touched }) => (
         <Form>
-          <Field name="firstName" className='form-control' />
-            <ErrorMessage name='firstName'>{msg => <div className='alert alert-warning' role='alert'>{msg}</div>}</ErrorMessage>
-          <Field name="lastName" />
-          {errors.lastName && touched.lastName ? (
-            <div>{errors.lastName}</div>
-          ) : null}
-          <Field name="email" type="email" />
-          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+          <div className='form-group'>
+            <label htmlFor='idNumber'>ID number</label>
+            <Field name="idNumber" className={'form-control' + (errors.idNumber && touched.idNumber ? ' is-invalid': '')} />
+            <ErrorMessage name='idNumber' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='firstName'>First name</label>
+            <Field name="firstName" className={'form-control' + (errors.firstName && touched.firstName ? ' is-invalid': '')} />
+            <ErrorMessage name='firstName' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='lastName'>Last name</label>
+            <Field name="lastName" className={'form-control' + (errors.firstName && touched.lastName ? ' is-invalid': '')} />
+            <ErrorMessage name='lastName' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='patronymic'>Patronymic</label>
+            <Field name="patronymic" className={'form-control' + (errors.patronymic && touched.patronymic ? ' is-invalid': '')} />
+            <ErrorMessage name='patronymic' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='birthPlace'>Birth place</label>
+            <Field name="birthPlace" className={'form-control' + (errors.birthPlace && touched.birthPlace ? ' is-invalid': '')} />
+            <ErrorMessage name='birthPlace' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='birthDate'>Birth date</label>
+            <Field name="birthDate" className={'form-control' + (errors.birthDate && touched.birthDate ? ' is-invalid': '')} />
+            <ErrorMessage name='birthDate' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='phone'>Phone number</label>
+            <Field name="phone" className={'form-control' + (errors.phone && touched.phone ? ' is-invalid': '')} />
+            <ErrorMessage name='phone' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='email'>Email</label>
+            <Field name="email" className={'form-control' + (errors.email && touched.email ? ' is-invalid': '')} />
+            <ErrorMessage name='email' component='div' className='invalid-feedback' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='rate'>Rate</label>
+            <Field name="rate" className={'form-control' + (errors.rate && touched.rate ? ' is-invalid': '')} />
+            <ErrorMessage name='rate' component='div' className='invalid-feedback' />
+          </div>
           <button type="submit">Submit</button>
         </Form>
       )}
